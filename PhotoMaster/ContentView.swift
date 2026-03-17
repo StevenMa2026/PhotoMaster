e5891a8b22a4b4d201da014bf4126caa89d745ce
Steven Ma <mshaobo@bu.edu>
2026-03-17 15:22:01 +0800

    Initial Commit

---
+++ PhotoMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoMaster
+//
+//  Created by Steven Ma on 2026/3/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoMaster/PhotoMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMasterApp.swift
+//  PhotoMaster
+//
+//  Created by Steven Ma on 2026/3/17.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
